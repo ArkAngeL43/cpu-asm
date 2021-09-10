9d6fd0a9eeb0015f074da6231f77cad2d4ffd6e8
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-09-10 18:35:48 +0000

    Add files via upload

---
+++ hard.asm
@@ -0,0 +1,19 @@
+section .data ;tell it to work
+        string db "Current Branded CPU => < XXXXXXXXXXXX >",0xA
+section .text
+global _start
+_start:
+       xor eax,eax
+       cpuid 
+       mov edi,string
+       mov [edi+25],ebx
+       mov [edi+29],edx
+       mov [edi+33],ecx
+       mov eax,4
+       mov ebx,1
+       mov ecx,string
+       mov edx,41
+       int 0x80
+       mov eax,1
+       xor ebx,ebx
+       int 0x80
