2c051558edad5758a7650c76474288f1a886604a
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-09-10 14:36:35 -0400

    Update hard.asm

--- hard.asm
+++ hard.asm
@@ -6,14 +6,14 @@ _start:
        xor eax,eax
        cpuid 
        mov edi,string
-       mov [edi+25],ebx
+       mov [edi+25],ebx ;why am i doing this 
        mov [edi+29],edx
        mov [edi+33],ecx
        mov eax,4
        mov ebx,1
        mov ecx,string
        mov edx,41
-       int 0x80
+       int 0x80 ; i hate myself for ever thinking i could be good at this lang XD
        mov eax,1
        xor ebx,ebx
-       int 0x80
+       int 0x80 ; finally that took a while 
